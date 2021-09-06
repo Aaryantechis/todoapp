17b53500fecdb7c0cb80c0b83a7775d838254d59
aaryantechis <aaryan@techis.io>
2021-09-07 04:43:47 +0530

    commit

---
+++ src/Todo.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Todo({ todo ,toggleTodo}) {
+    function handelTodoClick(){
+        toggleTodo(todo.id)
+    }
+  return <div>
+      <label>  
+          <input type="checkbox" checked={todo.complete} onChange={handelTodoClick} />   
+      {todo.name}
+      </label>
+      </div>;
+}
